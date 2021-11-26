8a9453eebef1ee6ef844d4c7afbdf54bf99da5fb
Max-Martin Sherie <maxmartinsherie@gmail.com>
2021-11-26 20:40:38 +0100

    Fixed Destroy heart glitch where the player could destroy a heart through walls

--- PC_Client_&_Server_Makina/Assets/Scripts/DestroyHeart.cs
+++ PC_Client_&_Server_Makina/Assets/Scripts/DestroyHeart.cs
@@ -19,22 +19,24 @@ public class DestroyHeart : MonoBehaviour
     void Update()
     {
         // Checking the raycast for objects of the heart layer
-        bool lookingAtHeart = Physics.Raycast(m_cameraTransform.position, m_cameraTransform.forward, out RaycastHit hit, m_range, m_layerMask);
+        bool lookingAtHeart = Physics.Raycast(m_cameraTransform.position, m_cameraTransform.forward, out RaycastHit hit, m_range);
+
+        if (!lookingAtHeart) lookingAtHeart = false;
+        else lookingAtHeart = hit.transform.gameObject.layer == (hit.transform.gameObject.layer | (1 << m_layerMask));
+        
         m_text.enabled = lookingAtHeart;
+        
+        if (!lookingAtHeart) return;
 
         //If the item is in sight and range and the key is down
         if (lookingAtHeart && Input.GetKeyDown(m_destroyKey))
-        {
             // Tell the server to destroy it
             if(hit.transform.gameObject.TryGetComponent(out HeartIdentifier hi))
             {
                 int heartIndex = hi.heartIndex;
                 NetworkClient.Send(new HeartBreak(){index = heartIndex});
             }
-            else
-            {
-                Debug.Log("there is a gameobject that is on the heart layer that doesn't have the heart identifier class ಠ_ಠ", hit.transform.gameObject);
-            }
-        }
+            else Debug.LogWarning("there is a gameobject that is on the heart layer that doesn't have the heart identifier class ಠ_ಠ", hit.transform.gameObject);
+        
     }
 }
