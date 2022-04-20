12830d76c10add7997a5d6cd486ba5cdb4861943
Max-Martin-Sherie <59953355+Max-Martin-Sherie@users.noreply.github.com>
2022-04-20 09:25:49 +0200

    LD MODS + GLOBAL LOD CONTROLLER + LIGHTMAP & COOKIE SUPPORT

--- PC_Client_&_Server_Makina/Assets/Shader/Customlightingtest/CustomLighting.hlsl
+++ PC_Client_&_Server_Makina/Assets/Shader/Customlightingtest/CustomLighting.hlsl
@@ -24,6 +24,9 @@ struct CustomLightingData {
     float smoothness;
     float ambientOcclusion;
     
+    // Baked lighting
+    float3 bakedGI;
+    float4 shadowMask;
     float fogFactor;
 };
 
@@ -72,6 +75,18 @@ Outputs CustomLightHandling(CustomLightingData d, Light light)
 }
 #endif
 
+
+Outputs CustomGlobalIllumination(CustomLightingData d) {
+    Outputs outputs;
+    float3 indirectDiffuse = d.albedo * d.bakedGI * d.ambientOcclusion;
+
+    float3 reflectVector = reflect(-d.viewDirectionWS, d.normalWS);
+    outputs.color = indirectDiffuse;
+    outputs.lightLevel =  0.3  * outputs.color.r + 0.59  * outputs.color.g + 0.11  * outputs.color.b;
+    
+    return outputs;
+}
+
 // The function to calculate the custom lighting
 Outputs CalculateCustomLighting(CustomLightingData d) {
     Outputs outputs;
@@ -86,16 +101,21 @@ Outputs CalculateCustomLighting(CustomLightingData d) {
     return outputs;
 #else
     // Get the main light
-    Light mainLight = GetMainLight(d.shadowCoord, d.positionWS, 1);
+    Light mainLight = GetMainLight(d.shadowCoord, d.positionWS,  d.shadowMask);
 
     // setting initial values
     outputs.color = d.albedo * d.ambientOcclusion;
     outputs.lightLevel = 0;
     
     // Shading the main light
-    Outputs results = CustomLightHandling(d,mainLight);
-    outputs.color += results.color; 
-    outputs.lightLevel += results.lightLevel; 
+    // Outputs results;
+    
+    Outputs results;
+    results = CustomGlobalIllumination(d);
+    Outputs results2 = CustomLightHandling(d,mainLight);
+
+    outputs.color += results.color + results2.color; 
+    outputs.lightLevel += results.lightLevel + results2.lightLevel; 
     
     #ifdef _ADDITIONAL_LIGHTS
     // Running the lighting for every light interacting wih the object
@@ -116,7 +136,7 @@ Outputs CalculateCustomLighting(CustomLightingData d) {
 
 // the function called by the custom node
 void CalculateCustomLighting_float(float3 Position, float3 Normal,float3 ViewDirection,
-    float3 Albedo, float Smoothness, float AmbientOcclusion,
+    float3 Albedo, float Smoothness, float AmbientOcclusion,float2 LightmapUV,
     out float3 Color, out float LightLevel) {
 
     CustomLightingData d;
@@ -141,6 +161,18 @@ void CalculateCustomLighting_float(float3 Position, float3 Normal,float3 ViewDir
         d.shadowCoord = TransformWorldToShadowCoord(Position);
     #endif
 
+    // The lightmap UV is usually in TEXCOORD1
+    // If lightmaps are disabled, OUTPUT_LIGHTMAP_UV does nothing
+    float2 lightmapUV;
+    OUTPUT_LIGHTMAP_UV(LightmapUV, unity_LightmapST, lightmapUV);
+    // Samples spherical harmonics, which encode light probe data
+    float3 vertexSH;
+    OUTPUT_SH(Normal, vertexSH);
+    // This function calculates the final baked lighting from light maps or probes
+    d.bakedGI = SAMPLE_GI(lightmapUV, vertexSH, Normal);
+    // This function calculates the shadow mask if baked shadows are enabled
+    d.shadowMask = SAMPLE_SHADOWMASK(lightmapUV);
+    
     d.fogFactor = ComputeFogFactor(positionCS.z);
     #endif
     Outputs outputs = CalculateCustomLighting(d);
