5fe3947dc1cdce6566fb4ecdfef0dcd0b360353f
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2022-06-02 17:34:39 +0200

    welp

--- VR_Client_Makina/Assets/Scripts/Synchronizers/SynchronizeInitialData.cs
+++ VR_Client_Makina/Assets/Scripts/Synchronizers/SynchronizeInitialData.cs
@@ -12,6 +12,8 @@ namespace Synchronizers
         /// <summary/> The PC Health data saved localy
         private int m_pcHealth;
         
+        private int m_maxPcHealth;
+        
         /// <summary/> The VR Health data saved localy
         private int m_vrHealth;
         
@@ -21,26 +23,27 @@ namespace Synchronizers
         private Coroutine m_healtheedbackCoroutine;
         [SerializeField]private float m_showHealthTime = 3f;
         
+        [SerializeField, Tooltip("One of them will be activated every time the VR player gets a kill (in order)")] private Transform[] m_uiPoint;
+        
         /// <summary/> Adding functions to the client delegate
         void Awake() => MyNetworkManager.OnReceiveInitialData += ReceiveInitialData;
 
         /// <summary/> Singleton type beat
-        private void Start()
-        {
+        private void Start() {
             if (instance == null) instance = this;
-            else
-            {
+            else {
                 gameObject.SetActive(false);
                 Debug.LogWarning("BROOOOOOOOOOOOOOOOOOO ! There are too many Singletons broda", this);
             }
-            
+
+            foreach (Transform point in m_uiPoint) point.gameObject.SetActive(false);
         }
 
         /// <summary/> Function called when the client received the initial data
         /// <param name="p_initialData"> The message sent by the server</param>
-        void ReceiveInitialData(InitialData p_initialData)
-        {
+        void ReceiveInitialData(InitialData p_initialData) {
             m_pcHealth = p_initialData.healthPcPlayer;
+            m_maxPcHealth = p_initialData.healthPcPlayer;
             m_vrHealth = p_initialData.healthVrPlayer;
             UpdateHealthText();
         }
@@ -48,8 +51,8 @@ namespace Synchronizers
         
         /// <summary/> The function called when the PC player looses Health
         /// <param name="p_healthLost"> the amount of health left (default : 1) </param>
-        public void LosePcHealth(int p_healthLost = 1)
-        {
+        public void LosePcHealth(int p_healthLost = 1) {
+            m_uiPoint[m_maxPcHealth - m_pcHealth].gameObject.SetActive(true);
             m_pcHealth -= p_healthLost;
             UpdateHealthText();
         }
@@ -57,14 +60,13 @@ namespace Synchronizers
         
         /// <summary/> The function called when the VR player looses Health
         /// <param name="p_healthLost"> the amount of health left (default : 1) </param>
-        public void LoseVrHealth(int p_healthLost = 1)
-        {
+        public void LoseVrHealth(int p_healthLost = 1) {
             m_vrHealth -= p_healthLost;
             UpdateHealthText();
         }
 
-        [ContextMenu("test")]
         /// <summary/> Updates both player's health on the PC GUI based on the local variables
+        [ContextMenu("test")]
         private void UpdateHealthText() {
             if(m_healtheedbackCoroutine != null) StopCoroutine(m_healtheedbackCoroutine);
             m_healtheedbackCoroutine = StartCoroutine(ShowHealthText());
