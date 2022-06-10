564760808e25e9242270764c6050be6e91596d4a
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2022-06-10 10:39:56 +0200

    yahooo

--- VR_Client_Makina/Assets/Scripts/Transition.cs
+++ VR_Client_Makina/Assets/Scripts/Transition.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 
 public class Transition : Synchronizer<Transition> {
 
-    [SerializeField] private Material m_transitionShader = null;
+    [SerializeField] private MeshRenderer m_rendererThatHaveShader = null;
     [SerializeField, Range(0.1f, 10f)] private float m_transitionDuration = 1f;
     private bool m_isTransitionning = false;
 
-    public Action a_transitionDone;
-    private static readonly int Filling = Shader.PropertyToID("Filling");
+    /// <summary>Is called once a transition is halfway done (thus black screen).<br/>Will be emptied after use.</summary>
+    public static Action a_transitionDone;
+    private static readonly int Filling = Shader.PropertyToID("_Filling");
 
 
     // Start is called before the first frame update
     void Start() {
-        if (m_transitionShader == null) Debug.LogError("Guess I'll stay cis if you don't want to serialize my transition...", this);
+        if (m_rendererThatHaveShader == null) Debug.LogError("Guess I'll stay cis if you don't want to serialize my transition...", this);
     }
 
     [ContextMenu("Transition!")]
@@ -27,29 +28,28 @@ public class Transition : Synchronizer<Transition> {
 
         m_isTransitionning = true;
         float elapsedTime = 0f;
-        m_transitionShader.SetFloat(Filling, 1f);
+        m_rendererThatHaveShader.sharedMaterial.SetFloat(Filling, 1f);
 
-        while (elapsedTime < (m_transitionDuration / 2f)) {
+        while (elapsedTime < (m_transitionDuration / 2f)) { // fade to black
             yield return null;
             elapsedTime += Time.deltaTime;
             
-            m_transitionShader.SetFloat(Filling, 1f-(elapsedTime / (m_transitionDuration / 2f)));
+            m_rendererThatHaveShader.sharedMaterial.SetFloat(Filling, 1f-(elapsedTime / (m_transitionDuration / 2f)));
         }
         
-        m_transitionShader.SetFloat(Filling, 0f);
+        m_rendererThatHaveShader.sharedMaterial.SetFloat(Filling, 0f);
         a_transitionDone?.Invoke();
+        a_transitionDone = null;
         elapsedTime = 0f;
         
-        while (elapsedTime < (m_transitionDuration / 2f)) {
+        while (elapsedTime < (m_transitionDuration / 2f)) { //fade to transparent
             yield return null;
             elapsedTime += Time.deltaTime;
             
-            m_transitionShader.SetFloat(Filling, elapsedTime / (m_transitionDuration / 2f));
+            m_rendererThatHaveShader.sharedMaterial.SetFloat(Filling, elapsedTime / (m_transitionDuration / 2f));
         }
         
         m_isTransitionning = false;
-        m_transitionShader.SetFloat(Filling, 1f);
+        m_rendererThatHaveShader.sharedMaterial.SetFloat(Filling, 1f);
     }
-    
-    
 }
