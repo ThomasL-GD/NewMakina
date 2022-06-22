406795135f9bc20a163519277f5d4e75419c10bd
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2022-06-22 11:11:09 +0200

    uy

--- VR_Client_Makina/Assets/Scripts/Network/Connexion Menu/CreditsButton.cs
+++ VR_Client_Makina/Assets/Scripts/Network/Connexion Menu/CreditsButton.cs
@@ -1,3 +1,5 @@
+using System;
+using CustomMessages;
 using UnityEngine;
 
 namespace Network.Connexion_Menu {
@@ -6,16 +8,30 @@ namespace Network.Connexion_Menu {
 
         [SerializeField] private GameObject[] m_creditsObjects;
         [SerializeField] private GameObject[] m_objectsToUnactiveOnCredits;
+        [SerializeField] private bool m_isCredits;
 
         public override void OnBeingActivated() {
             base.OnBeingActivated();
 
             Transition.a_transitionDone += ShowCredits;
             Transition.Instance.StartTransition();
+
+            if (m_isCredits) {
+                MyNetworkManager.OnReadyToGoIntoTheBowl += Reset;
+                MyNetworkManager.OnReceiveInitiateLobby += Reset;
+            }
         }
+
+        private void Reset(InitiateLobby p_p_ready) => Reset();
+
+        private void Reset(ReadyToGoIntoTheBowl p_p_ready) => Reset();
 
         private void ShowCredits() {
             foreach (GameObject obj in m_creditsObjects) obj.SetActive(true);
+            foreach (GameObject obj in m_objectsToUnactiveOnCredits) obj.SetActive(false);
+        }
+
+        private void Reset() {
             foreach (GameObject obj in m_objectsToUnactiveOnCredits) obj.SetActive(false);
         }
     }
